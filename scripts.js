2f37d0032a987d7cf5faf793f57792df1a5a7c61
Brian <brian.tonkei@xdesign.com>
2023-12-15 16:56:51 +0000

    _Alika_

---
+++ scripts.js
@@ -0,0 +1,19 @@
+console.log ('test console logging');
+
+console.log('does my code work')
+
+
+
+
+let Name = 'Brian';
+Name= 'Tonkei Brian'
+console.log (Name);
+
+
+
+let Place;
+console.log (Place);
+
+
+const Country = 'kenya';
+console.log (Country);
